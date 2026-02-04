16cc58d65d737a4f77fd4342f8164f906afed6a7
Harshit Padha <harshit@encresa.com>
2026-02-04 14:39:46 +0530

    Configure for custom domain encresa.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://encresa.github.io',
-    base: '/aura-website',
+    site: 'https://encresa.com',
     output: 'static',
     build: {
         assets: '_assets'
